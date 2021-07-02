be37524a934e6e84db70c815183a2524c7b80c54
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-07-02 16:44:54 +0900

    [Add]attack

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 class Brave
-
   attr_reader :name, :offense, :defense
-
   attr_accessor :hp
+
+  SPECIAL_ATTACK_CONSTANT = 1.5
 
   def initialize(**params)
     @name = params[:name]
@@ -11,17 +11,79 @@ class Brave
     @defense = params[:defense]
   end
 
-  brave = Brave.new(name: "リンク", hp: 500, offense: 150, defense: 100)
+  def attack(monster)
+    puts "#{@name}の攻撃"
 
-  puts <<~TEXT
-  NAME：#{brave.name}
-  HP：#{brave.hp}
-  OFFENSE：#{brave.offense}
-  DEFENSE：#{brave.defense}
-  TEXT
+    attack_num = rand(4)
 
-  brave.hp -= 30
+    if attack_num == 0
+      puts "必殺攻撃"
+      damage = calculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
 
-  puts "#{brave.name}はダメージを受けた!　残りHPは#{brave.hp}だ"
+    monster.hp -= damage
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
+  end
 
 end
+
+class Monster
+  attr_reader :offense, :defense
+  attr_accessor :hp, :name
+
+  POWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
+  end
+
+  def attack(brave)
+
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+
+    puts "#{@name}の攻撃"
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+  def transform
+    transform_name = "ドラゴン"
+
+    puts <<-EOS
+      #{@name}は怒っている
+      #{@name}は#{transform_name}に変身した
+    EOS
+
+    @offense *= POWER_UP_RATE
+    @name = transform_name
+  end
+
+end
+
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: "スライム", hp: 200, offense: 200, defense: 100)
+brave.attack(monster)
+monster.attack(brave)
